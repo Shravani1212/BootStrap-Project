936aaddef9cace5bbcc2dd865ee67b6c669c1144
Shravani1212 <kasthurishravani1212@gmail.com>
2023-06-27 16:05:32 +0530

    committed

--- foodmunch.js
+++ foodmunch.js
@@ -1,26 +1,5 @@
 $(document).ready(function() {
-  function placeOrder() {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var selectedFoodItems = [];
-    $("input[type='checkbox']:checked").each(function() {
-      selectedFoodItems.push($(this).val());
-    });
-    var paymentMethod = $("#payment").val();
-    var address = $("#address").val();
-
-    // Perform further processing or validation if needed
-
-    // Display selected food items
-    $(".selected-food-items").html("Selected Food Items: " + selectedFoodItems.join(", "));
-
-    // Show alert message
-    var message = "Order Placed!\n\nName: " + name + "\nEmail: " + email + "\nSelected Food Items: " + selectedFoodItems.join(", ") + "\nPayment Method: " + paymentMethod + "\nDelivery Address: " + address;
-    alert(message);
-
-    // Close the modal
-    $("#orderModal").modal("hide");
-  }
+  
   $("#glitter-button").click(function() {
     // Generate a random position within the container
     var containerWidth = $("#glitter-container").width();
@@ -40,11 +19,8 @@ $(document).ready(function() {
     // Display the image after a delay of 2000 milliseconds (2 seconds) with a fading effect
     var image = $(".thanking-customers-section-img");
     image.attr("src", "thanking-customers-section-img.jpg");
-    image.fadeIn(500, function() {
-      
       image.attr("src", "OIP.jpg"); // Set the open gift box image after the fade-in animation
-      image.animate({ width: "60%", height: "60%" }, 2000); // Adjust the animation duration as desired
-    }); 
+     
     
     // Set initial opacity to 0
     
@@ -63,3 +39,25 @@ $(document).ready(function() {
   glitter.style.top = positionTop + "px";
   $("#glitter-container2").append(glitter);
 });
+function placeOrder() {
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var selectedFoodItems = [];
+  $("input[type='checkbox']:checked").each(function() {
+    selectedFoodItems.push($(this).val());
+  });
+  var paymentMethod = $("#payment").val();
+  var address = $("#address").val();
+
+  // Perform further processing or validation if needed
+
+  // Display selected food items
+  $(".selected-food-items").html("Selected Food Items: " + selectedFoodItems.join(", "));
+
+  // Show alert message
+  var message = "Order Placed!\n\nName: " + name + "\nEmail: " + email + "\nSelected Food Items: " + selectedFoodItems.join(", ") + "\nPayment Method: " + paymentMethod + "\nDelivery Address: " + address;
+  alert(message);
+
+  // Close the modal
+  $("#orderModal").modal("hide");
+}

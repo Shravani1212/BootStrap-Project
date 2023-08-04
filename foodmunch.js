7580321cf820f252d9eac2efec0e86e4a1d1207c
Shravani1212 <kasthurishravani1212@gmail.com>
2023-08-04 21:43:52 +0530

    commit

--- foodmunch.js
+++ foodmunch.js
@@ -25,40 +25,31 @@ $(document).ready(function() {
     let address=prompt('Your order has been confirmed please eneter your your address')
     if(address!==null){
       alert(`Thank you for ordering 
-      Go and redeem your gift with${Math.ceil(Math.random)}abc#
+      Go and redeem your gift with  
+      ${Math.ceil(Math.random())}#abc code
     We will shortly place your order at location ${address}
     hope you will enjoy the meal!!!
      happy Meal;;;`)
       
     }
     else{
-      alert(`Sorry You haven't orders anything`)
+      alert(`Sorry You haven't ordered anything`)
     }
   })
 
   $("#glitter-button").click(function() {
-    // Generate a random position within the container
-    var containerWidth = $("#glitter-container").width();
-    var containerHeight = $("#glitter-container").height();
-    var positionLeft = Math.floor(Math.random() * containerWidth);
-    var positionTop = Math.floor(Math.random() * containerHeight);
-
-    // Create a new glitter element
-    var glitter = document.createElement("div");
-    glitter.classList.add("glitter");
-    glitter.style.left = positionLeft + "px";
-    glitter.style.top = positionTop + "px";
-
-    // Add the glitter element to the container
-    $("#glitter-container").append(glitter);
+    
 
     // Display the image after a delay of 2000 milliseconds (2 seconds) with a fading effect
-    var image = $(".thanking-customers-section-img");
-    image.attr("src", "thanking-customers-section-img.jpg");
-      image.attr("src", "OIP.jpg"); // Set the open gift box image after the fade-in animation
+    if(prompt("enter coupon code")!==null){
+      var image = $(".thanking-customers-section-img");
+      image.attr("src", "thanking-customers-section-img.jpg");
+        image.attr("src", "OIP.jpg"); 
+    };
+   
      
     
-    // Set initial opacity to 0
+   
     
   });
 
@@ -88,7 +79,7 @@ function placeOrder() {
   $(".selected-food-items").html("Selected Food Items: " + selectedFoodItems.join(", "));
 
  
-  var message = "Order Placed!\n\nName: " + name + "\nEmail: " + email + "\nSelected Food Items: " + selectedFoodItems.join(", ") + "\nPayment Method: " + paymentMethod + "\nDelivery Address: " + address +"Thank You For Ordering Enjoy the taste!!! have a Nice day!!! Go and redeem your gift";
+  var message = "Order Placed!\n\nName: " + name + "\nEmail: " + email + "\nSelected Food Items: " + selectedFoodItems.join(", ") + "\nPayment Method: " + paymentMethod + "\nDelivery Address: " + address +`Thank You For Ordering Enjoy the taste!!! have a Nice day!!! Go and redeem your gift ${Math.ceil(Math.random())}#abc  code`;
   alert(message);
 //  alert(<img src="food truck.jpg"/>)
  
